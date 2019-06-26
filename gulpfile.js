bed37cb963204acd669dfa39a5ea003544b07d89
mahdiafzal <mahdiafzal@gmail.com>
2019-06-26 18:31:53 +0430

    added gulp css clean + change gulp run

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,59 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var browserSync = require('browser-sync')
-var sourcemaps = require('gulp-sourcemaps')
- 
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sourcemaps = require('gulp-sourcemaps'),
+  minifyCss = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  browserSync = require('browser-sync').create();
+
+
+
 gulp.task('sass', function () {
-  return gulp.src('./src/sass/**/*.scss')
+  // sass directory
+  return gulp.src('./src/sass/*scss')
+    .pipe(sass())
+    //outputstyle (nested, compact, expanded, compressed)
+    .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+    // sourcemaps
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(sourcemaps.write('./'))
+    // sourcemaps output directory
+    .pipe(sourcemaps.write(('./maps')))
+    // css output directory
+    .pipe(gulp.dest('./docs/css')),
+    // watch file
+    gulp.watch('./sass/*.scss', ['sass']);
+});
+
+
+// minify css (merge + autoprefix + rename)
+gulp.task('minify-css', function () {
+  return gulp.src('./docs/css/style.css')
+    .pipe(minifyCss())
+    // autoprefixer
+    .pipe(autoprefixer({
+      browsers: ['last 15 versions'],
+      cascade: false
+    }))
+    // minify output directory
+    .pipe(rename('style.min.css'))
     .pipe(gulp.dest('./docs/css'))
-})
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/**/*.scss', ['sass'])
-})
+    // browser sync
+    .pipe(browserSync.reload({ stream: true })),
+    // watch file
+    gulp.watch('./docs/css/style.css', ['minify-css']);
+});
 
-var server = browserSync.create()
-gulp.task('server', () => 
-  server.init({
-    ui: false,
+
+// sass/css browser tracking
+gulp.task('browser-sync', function () {
+  browserSync.init({
     server: {
-      baseDir: ['./docs/'],
-      index: 'index.html'
+      baseDir: './docs/'
     }
-  })
-)
+  });
+  // watch html
+  gulp.watch('./*.html').on('change', browserSync.reload);
+});
 
-gulp.task('serverBrowserSync', () =>
-  server.reload()
-)
-
-gulp.task('server:watch', () => 
-  gulp
-    .watch([
-      './docs/css/*',
-      './docs/dummy/*',
-      './docs/favicons/*',
-      './docs/fonts/*',
-      './docs/images/*',
-      './docs/js/*',
-      './docs/*',
-    ], ['serverBrowserSync'])
-)
-
-gulp.task('default', ['sass', 'sass:watch', 'server', 'server:watch'])
+// gulp default (sass, minify-css, browser-sync) method
+gulp.task('default', ['sass', 'minify-css', 'browser-sync']);
